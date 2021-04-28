13527e0d60868b3cd7063714ab40f9f1f04cb16f
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2021-04-28 21:30:22 +0200

    Initial commit

---
+++ DinoRace/Data/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinoRace.Data
+{
+    public class Player
+    {
+        public string Color { get; set; }
+        public int Position { get; set; } = 0;
+
+    }
+}
